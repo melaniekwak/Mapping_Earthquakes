9ae1ec51dd54c4e5709a1584238ca72e77a5ac59
melaniekwak <melaniekwak13@gmail.com>
2021-04-20 22:07:37 -0700

    markers

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -16,11 +16,24 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 // let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
 // Add marker to map for Los Angeles, CA
-L.circleMarker([34.0522, -118.2437], {
-  radius: 300,
-  color: "black",
-  fillcolor: '#ffffa1'
-}).addTo(map);
+// L.circleMarker([34.0522, -118.2437], {
+//   radius: 300,
+//   color: "black",
+//   fillcolor: '#ffffa1'
+// }).addTo(map);
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+  console.log(city)
+  L.circleMarker(city.location, {
+    radius: city.population/100000
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
